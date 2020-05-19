0b9c1caf22dd576ed8a5fbfc16363bb3e2df122d
Michelle McCormack <michelle@castingcoin.com>
2020-05-19 20:08:53 -0300

    Reforma DER

--- SegundaEntrega/tp.productor.sql
+++ SegundaEntrega/tp.productor.sql
@@ -1,19 +1,26 @@
-insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Yacireta', 3000, 3100, '1992-04-10');
-insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Atucha', 692, 745, '2014-06-27');
-insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Metsamor', 375, 408, '1980-05-03');
-insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Doel', 1039, 1090, '1985-07-01');
-insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Tihange', 1046, 1089, '1985-09-01');
-insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Angra', 1275, 1350, '2001-02-01');
-insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Kozloduy', 963, 1000, '1988-12-30');
-insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Chinon', 905, 954, '1985-07-01');
-insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Cruas', 915, 956, '1985-07-01');
-insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Kaigas', 202, 220, '1985-07-01');
-insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Genkai', 1127, 1180, '1994-06-01');
-insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Ikata', 846, 890, '1990-10-1');
-insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Oi', 1127, 1180, '1991-12-18');
-insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Sendai', 846, 890, '1985-07-01');
-
-
-
-
-
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Yacireta', 3100, 3000, '1992-04-10');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Atucha', 745, 692, '2014-06-27');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Metsamor', 408, 375, '1980-05-03');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Doel', 1090, 1039, '1985-07-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Tihange', 1089, 1046, '1985-09-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Angra', 1350, 1275, '2001-02-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Kozloduy', 1000, 963, '1988-12-30');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Chinon', 954, 905, '1985-07-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Cruas', 956,915, '1985-07-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Kaigas', 220, 202, '1985-07-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Genkai', 1180, 1127, '1994-06-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Ikata', 890, 846, '1990-10-1');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Oi', 1180, 1127, '1991-12-18');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Sendai', 890, 846, '1985-07-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Three Gorges', 22500, 22000, '2008-01-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Itaipu', 14000, 13000, '1984-01-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Belo Monte', 11200, 11000, '2016-01-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Guri', 10235, 10200, '1978-01-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Tucurui', 8370, 8300, '1984-01-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Grand Coulee', 6809, 6800, '1942-01-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Tarbela', 4888, 4800, '1978-01-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Bratsk',3840, 3800, '1978-01-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Jirau', 3750, 3700, '2014-01-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Santo Antonio', 3580, 3500, '2012-01-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Macagua', 3167, 3100, '1961-01-01');
+insert into tp.productor (nombre, produccion_maxima, produccion_media, fecha_funcionamiento) values ('Niagara Falls', 10235, 10200, '1961-01-01');
